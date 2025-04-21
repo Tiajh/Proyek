9367047ff5a7205087fdf38e188d08cc4528735a
Tiajh <tiahenim25@gmail.com>
2025-04-21 13:33:55 +0700

    selesai

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,20 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\View\View;
 
 class ProductController extends Controller
 {
     private $products = [
-        1 => ['name' => 'Kaos Laravel', 'price' => 150000],
-        2 => ['name' => 'Stiker Coding', 'price' => 25000],
-        3 => ['name' => 'Notebook Dev', 'price' => 50000],
+        1 => ['name' => 'Kaos Laravel', 'price' => 150000, 'jenis'=> 'Pakaian', 'merek' => 'inkalum', 'material' => 'kain'],
+        2 => ['name' => 'Stiker Koding', 'price' => 25000, 'jenis'=> 'Pakaian', 'merek' => 'inkalum', 'material' => 'stiker'],
+        3 => ['name' => 'Notebook Dev', 'price' => 50000, 'jenis'=> 'Pakaian', 'merek' => 'inkalum', 'material' => 'kertas'],
     ];
 
-    // Menampilkan daftar produk
     public function index()
     {
-        $products =  $this ->products;
+        $products = $this->products;
         return view('products.index', compact('products'));
     }
+        public function show($id)
+
+    {
+        $products = $this->products[$id];
+        return view('products.detail', compact('products'));
+}
 }
