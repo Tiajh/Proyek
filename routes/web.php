4826a8dcaad6c88f918fa8933d78eb57949e7a22
Tiajh <tiahenim25@gmail.com>
2025-05-24 19:58:14 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,18 @@ use App\Http\Controllers\AdminController;
 // });
 
 
-Route::get('/', [ProductController::class,'index']);
-Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.detail');
-Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
-Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
-Route::get('/Contact', [ContactController::class, 'create']);
-Route::post('/Contact', [ContactController::class, 'store'])->name('contact.store');
-Route::get('/admin', [AdminController::class, 'create'])->name('admin.create');
-Route::post('/admin', [AdminController::class, 'store'])->name('admin.store');
+//Route::get('/', [ProductController::class,'index']);
+//Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.detail');
+
+//Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+//Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
+
+//Route::get('/Contact', [ContactController::class, 'create']);
+//Route::post('/Contact', [ContactController::class, 'store'])->name('contact.store');
+
+//Route::get('/admin', [AdminController::class, 'create'])->name('admin.create');
+//Route::post('/admin', [AdminController::class, 'store'])->name('admin.store');
+
+Route::get('/Home', function () {
+    return view('Home');
+});
